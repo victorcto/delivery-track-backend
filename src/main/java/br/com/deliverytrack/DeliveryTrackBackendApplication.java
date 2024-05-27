00eb9b8911a811f22184efff35b7b7702ee0f96c
Victor Couto <victorcto@outlook.com>
2024-05-27 19:54:41 -0300

    criado projeto do backend

---
+++ src/main/java/br/com/deliverytrack/DeliveryTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.deliverytrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTrackBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryTrackBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/deliverytrack/DeliveryTrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deliverytrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTrackBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
